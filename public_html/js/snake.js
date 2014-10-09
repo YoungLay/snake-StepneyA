09521303d9144809b49ae44bc38ee0dea6f34daa
Aalaysia Stepney <1stepneyaalaysia@gmail.com>
2014-10-09 15:00:42 -0700

    milestone3

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,1 +1,18 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
