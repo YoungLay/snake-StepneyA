dccae190fed2fd166e94cb2e31563d18bdd8f783
Aalaysia Stepney <1stepneyaalaysia@gmail.com>
2014-10-10 14:59:24 -0700

    video 22

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,18 +1,70 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize ();
+snakeInitialize ();
+setInterval(gameLoop, 2000/30);
+
+
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
-    
+    gameDraw ();
+    snakeUpdate ();
+    snakeDraw ();
 }
 
+
 function gameDraw() {
+    context.fillStyle = "rgb(180, 250, 213)";
+    context.fillRect (0, 0, screenWidth, screenHeight);
+}
+
+
+function snakeIntialize(){
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
     
+    for(var index = 0; index < snakeLength; index++){
+        snake.push({
+            x: index,
+            y: 0
+        });
+    }
+}
+
+
+function snakeDraw(){
+    for(var index = 0; index <snake.length; index++){
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize);
+    }
+    }
+
+
+function snakeUpdate(){
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
 }
